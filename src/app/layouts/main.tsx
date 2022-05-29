c33e47cc8f821f21296ddad742a68fee2e248fda
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2022-05-29 10:47:53 +0500

    navbar

--- src/app/layouts/main.tsx
+++ src/app/layouts/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/ui/navbar'
 
 const Main = () => {
 	return (
-		<div className='container'>
-			<Outlet />
-		</div>
+		<>
+			<Navbar />
+			<div className='container'>
+				<Outlet />
+			</div>
+		</>
 	)
 }
 
